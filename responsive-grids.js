c5879260be838d14ef76dd2e428b116ade7b386f
Raul Murciano <raul@murciano.net>
2011-07-21 20:17:39 -0700

    Tiny improvements when assigning .first-line

--- responsive-grids.js
+++ responsive-grids.js
@@ -1,35 +1,35 @@
 var ResponsiveGrids = (function(){
-	
-	var AUTHOR = "Sergi Meseguer @zigotica";
-	var URL = "http://zigotica.github.com/Responsive-Grids/";
-	
-	
-	var grids = document.createElement("div");
-	grids.id = "grid";
 
-	var gv = document.createElement("div");
-	gv.className = "vert-container";
+        var AUTHOR = "Sergi Meseguer @zigotica";
+        var URL = "http://zigotica.github.com/Responsive-Grids/";
 
-	for (var i = 0; i < 50; i++) {
-		var gvi = document.createElement("div");
-		if(i==0) gvi.className = "vert first-line cols"+parseInt(i+1);
-		else gvi.className = "vert cols"+parseInt(i+1);
-	
-		gv.appendChild(gvi);
-	}
-	grids.appendChild(gv);
 
-	var gh = document.createElement("div");
-	gh.className = "horiz-container";
+        var grids = document.createElement("div");
+        grids.id = "grid";
 
-	for (var i = 0; i < 200; i++) {
-		var ghi = document.createElement("div");
-		if(i==0) ghi.className = "horiz first-line";
-		else ghi.className = "horiz";
-	
-		gh.appendChild(ghi);		
-	}
-	grids.appendChild(gh);
-		
-	document.body.insertBefore(grids, document.body.childNodes[0]);
+        var gv = document.createElement("div");
+        gv.className = "vert-container";
+
+        for (var i = 0; i < 50; i++) {
+                var gvi = document.createElement("div");
+                gvi.className = "vert cols" + parseInt(i+1);
+                if(i==0) gvi.className += " first-line";
+
+                gv.appendChild(gvi);
+        }
+        grids.appendChild(gv);
+
+        var gh = document.createElement("div");
+        gh.className = "horiz-container";
+
+        for (var i = 0; i < 200; i++) {
+                var ghi = document.createElement("div");
+                ghi.className = "horiz"
+                if(i==0) ghi.className += " first-line";
+
+                gh.appendChild(ghi);
+        }
+        grids.appendChild(gh);
+
+        document.body.insertBefore(grids, document.body.childNodes[0]);
 })()
